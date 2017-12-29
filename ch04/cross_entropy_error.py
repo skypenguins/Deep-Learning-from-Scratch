06b352b6007b892e95cbe337aa40923e9582b913
skypenguins <monomeg.lab@gmail.com>
2017-12-29 23:20:11 +0900

    Ended 4.2

---
+++ ch04/cross_entropy_error.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def cross_entropy_error(y, t):
+    delta = 1e-7
+    return -np.sum(t * np.log(y + delta))
---
+++ ch04/mean_squared_error.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y-t)**2)

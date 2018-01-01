e28f8c4558d14fd9761adfcef781f6bda67e76ff
skypenguins <monomeg.lab@gmail.com>
2018-01-02 00:38:21 +0900

    Ended 6.2

--- ch05/two_layer_net.py
+++ ch05/two_layer_net.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 import sys, os
 sys.path.append(os.pardir)
-from common.functions import *
+import numpy as np
+from common.layers import *
 from common.gradient import numerical_gradient
+from collections import OrderedDict
 
 
 class TwoLayerNet:
 
-    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01):
         # init weight
         self.params = {}
         self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
@@ -15,27 +17,29 @@ class TwoLayerNet:
         self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
         self.params['b2'] = np.zeros(output_size)
 
+        # generate layer
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+
+        self.lastLayer = SoftmaxWithLoss()
+
     def predict(self, x):
-        W1, W2 = self.params['W1'], self.params['W2']
-        b1, b2 = self.params['b1'], self.params['b2']
+        for layer in self.layers.values():
+            x = layer.forward(x)
 
-        a1 = np.dot(x, W1) + b1
-        z1 = sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        y = softmax(a2)
+        return x
 
-        return y
-
-    # x:input data, t:label data
+    # x:input data, t:teacher data
     def loss(self, x, t):
         y = self.predict(x)
-
-        return cross_entropy_error(y, t)
+        return self.lastLayer.forward(y, t)
 
     def accuracy(self, x, t):
         y = self.predict(x)
         y = np.argmax(y, axis=1)
-        t = np.argmax(t, axis=1)
+        if t.ndim != 1 : t = np.argmax(t, axis=1)
 
         accuracy = np.sum(y == t) / float(x.shape[0])
         return accuracy
@@ -52,6 +56,26 @@ class TwoLayerNet:
 
         return grads
 
+    def gradient(self, x, t):
+        # forward
+        self.loss(x, t)
+
+        # backward
+        dout = 1
+        dout = self.lastLayer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        # settings
+        grads = {}
+        grads['W1'], grads['b1'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
+        grads['W2'], grads['b2'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
+
+        return grads
+
 print("net.params")
 net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
 print(net.params['W1'].shape)
@@ -61,12 +85,13 @@ print(net.params['b2'].shape)
 
 x = np.random.rand(100, 784) # dummy input data (100)
 y = net.predict(x)
-print("predict: " + str(y))
+print("predict:")
+print(str(y))
 
 print("grads")
 t = np.random.rand(100, 10) # dummy teacher data (100)
 
-grads = net.numerical_gradient(x, t)
+grads = net.gradient(x, t)
 print(grads['W1'].shape)
 print(grads['b1'].shape)
 print(grads['W2'].shape)
